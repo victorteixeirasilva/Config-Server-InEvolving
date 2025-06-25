bf4b7422903c03e9bdb4e332529ffdaaeb0d7fac
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-06-25 17:24:43 -0300

    Desenvolvendo config server

---
+++ config-service/src/test/java/tech/inovasoft/inevolving/ms/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.inovasoft.inevolving.ms.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
